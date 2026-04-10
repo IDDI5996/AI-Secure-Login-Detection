009741a6d682b5b02193ee5a4930b4066d472b57
Iddi B Hemedi <iddibakari171@gmail.com>
2026-04-10 16:34:03 +0300

    Solution to IP address and unknown location

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,11 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Add TrustProxies to global middleware stack
+        $middleware->append(\App\Http\Middleware\TrustProxies::class);
+        
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
     })
-    
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();

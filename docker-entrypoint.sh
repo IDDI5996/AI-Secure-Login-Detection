def1e40d04658b1c63b34c182d3a27d2baa689ea
Iddi B Hemedi <iddibakari171@gmail.com>
2026-04-12 01:11:18 +0300

    docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
 set -e
 
-# Run migrations safely
+# Create storage link (safe to run multiple times)
+php artisan storage:link
+
+# Run migrations (--force for production, ignore errors if already up-to-date)
 php artisan migrate --force || true
 
 # Start Apache in foreground
